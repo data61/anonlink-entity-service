c700d9702d3dc24017b0a94fde555b05516862eb
Brian Thorne <brian.thorne@data61.csiro.au>
2016-12-19 14:32:44 +1100

    Create v1.2 of entity app. Update kubernetes configs to include redis and backend.

--- tools/build.sh
+++ tools/build.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 cd backend
 docker pull python:3.5
-docker build -t quay.io/n1analytics/entity-app:v1 .
+docker build -t quay.io/n1analytics/entity-app:v1.2 .
 cd ../frontend
-docker build -t quay.io/n1analytics/entity-nginx:v1 .
+docker build -t quay.io/n1analytics/entity-nginx:v1.2 .
 cd ../db-server
 docker build -t quay.io/n1analytics/entity-db-server:v1 .
 cd ..
--- tools/upload.sh
+++ tools/upload.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 echo "Pushing images to quay.io"
-docker push quay.io/n1analytics/entity-app:v1
-docker push quay.io/n1analytics/entity-nginx:v1
+docker push quay.io/n1analytics/entity-app:v1.2
+docker push quay.io/n1analytics/entity-nginx:v1.2
 docker push quay.io/n1analytics/entity-db-server:v1
 echo "Success"
