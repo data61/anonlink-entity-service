d29c4a9e4f376088127bbf98a44a1fb8d7503dc8
Brian Thorne <brian.thorne@data61.csiro.au>
2017-01-10 10:56:28 +1100

    Add bash script to start and test the entity service using docker-compose

---
+++ tools/ci_test.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env bash
+
+# Stop and remove any existing entity service
+docker-compose -f tools/docker-compose.yml -f tools/ci.yml -p entityservicetest down
+docker-compose -f tools/docker-compose.yml -f tools/ci.yml -p entityservicetest rm
+
+# Build the images
+./tools/build.sh
+
+# Start all the containers
+docker-compose -f tools/docker-compose.yml -f tools/ci.yml -p entityservicetest up -d
+
+sleep 5
+docker ps
+
+# Follow the logs of the testing container
+docker logs -t -f entityservicetest_ci_1
+
+# Raise the exit code of the tests
+exit `docker inspect --format='{{.State.ExitCode}}' entityservicetest_ci`
+
+sleep 5
+cd deploy/entity-service
+docker-compose -f tools/docker-compose.yml -f tools/ci.yml -p entityservicetest down
