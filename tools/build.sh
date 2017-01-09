5fb1116401bd6ce6820c590077fcbe16aea645c6
Brian Thorne <Brian.Thorne@data61.csiro.au>
2017-01-09 15:58:07 +1100

    Make sure script for building entity service docker containers fails if a single command fails

--- tools/build.sh
+++ tools/build.sh
@@ -1,7 +1,8 @@
 #!/usr/bin/env bash
+set -e
 cd backend
 docker pull python:3.5
-docker build -t quay.io/n1analytics/entity-app:v1.2.3 .
+docker build -t quay.io/n1analytics/entity-app:v1.2.4 .
 cd ../frontend
 docker build -t quay.io/n1analytics/entity-nginx:v1.2 .
 cd ../db-server
