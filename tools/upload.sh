917c67dce64b55ac6d9c066bf7032c99ad13d65b
Brian Thorne <brian.thorne@data61.csiro.au>
2017-01-10 11:28:02 +1100

    Get docker image version from VERSION file

--- tools/upload.sh
+++ tools/upload.sh
@@ -1,7 +1,10 @@
 #!/usr/bin/env bash
 set -e
+script_name=$(basename "$0")
+cd $(dirname "$0")
+
 echo "Pushing images to quay.io"
-export APPVERSION=$(cat VERSION)
+export APPVERSION=$(cat ../backend/VERSION)
 docker push quay.io/n1analytics/entity-app:$APPVERSION
 docker push quay.io/n1analytics/entity-nginx:v1.2
 docker push quay.io/n1analytics/entity-db-server:v1
