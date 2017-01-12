52a442f6e63b61c1ebbbc0c43719673302e36a47
Brian Thorne <brian.thorne@data61.csiro.au>
2017-01-12 12:58:39 +1100

    Update entity service ci test script

--- tools/ci_test.sh
+++ tools/ci_test.sh
@@ -11,16 +11,21 @@ docker-compose -f docker-compose.yml -f ci.yml -p entityservicetest down -v
 # Build the images
 ./build.sh
 
-
-
-# Start all the containers
+# Start all the containers including tests
 docker-compose -f docker-compose.yml -f ci.yml -p entityservicetest up -d
 
 sleep 5
-docker ps
 
 # Follow the logs of the testing container
 docker logs -t -f entityservicetest_ci_1
+
+echo "Other Logs:"
+
+docker logs entityservicetest_es_backend_1
+docker logs entityservicetest_es_nginx_1
+
+echo "Cleanup:"
+./ci_cleanup.sh
 
 # Raise the exit code of the tests
 exit `docker inspect --format='{{.State.ExitCode}}' entityservicetest_ci_1`
