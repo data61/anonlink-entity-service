b8f6f39a94816d6c685b77c7fc48d7045029a613
Brian Thorne <brian.thorne@data61.csiro.au>
2017-01-12 11:04:10 +1100

    Only upload docker images from master branch

--- tools/upload.sh
+++ tools/upload.sh
@@ -3,9 +3,13 @@ set -e
 script_name=$(basename "$0")
 cd $(dirname "$0")
 
-echo "Pushing images to quay.io"
-export APPVERSION=$(cat ../backend/VERSION)
-docker push quay.io/n1analytics/entity-app:$APPVERSION
-docker push quay.io/n1analytics/entity-nginx:v1.2.3
-docker push quay.io/n1analytics/entity-db-server:v1
-echo "Success"
+if [ "$BRANCH_NAME" = "master" ];
+then
+    echo "Pushing images to quay.io";
+    export APPVERSION=$(cat ../backend/VERSION);
+    docker push quay.io/n1analytics/entity-app:$APPVERSION;
+    docker push quay.io/n1analytics/entity-nginx:v1.2.3;
+    docker push quay.io/n1analytics/entity-db-server:v1;
+else
+    echo "Skipping upload as not in master branch";
+fi
