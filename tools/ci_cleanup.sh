0ae3eca22da148a613b5fcb19eebb1438366fd3d
Brian Thorne <brian@thorne.link>
2017-03-02 17:24:26 +1300

    Try store the tests exit code from within docker to raise after cleanup so the ci status correctly represents the results.

--- tools/ci_cleanup.sh
+++ tools/ci_cleanup.sh
@@ -3,4 +3,9 @@
 set -e
 cd $(dirname "$0")
 
+# Raise the exit code of the tests
+exit_code=`docker inspect --format='{{.State.ExitCode}}' entityservicetest_ci_1`
+
 docker-compose -f docker-compose.yml -f ci.yml -p entityservicetest down -v
+
+exit exit_code
