993cf20d30325a7900175c60250f659587c9911e
Brian Thorne <brian@thorne.link>
2017-03-17 16:29:36 +1100

    Add support for CORS to nginx

--- tools/upload.sh
+++ tools/upload.sh
@@ -10,7 +10,7 @@ then
     echo "Pushing images to quay.io";
     export APPVERSION=$(cat ../backend/VERSION);
     docker push quay.io/n1analytics/entity-app:${APPVERSION};
-    docker push quay.io/n1analytics/entity-nginx:v1.2.6;
+    docker push quay.io/n1analytics/entity-nginx:v1.2.7;
 else
     echo "Uploading with label: ${BRANCH_NAME}";
     docker push quay.io/n1analytics/entity-app:${BRANCH_NAME};
