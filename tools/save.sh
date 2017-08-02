0666e5aa7d906cc810f978258b0a0a09baf908ff
Brian Thorne <brian.thorne@csiro.au>
2017-08-02 16:27:56 +1000

    Version bump v1.5.5

--- tools/save.sh
+++ tools/save.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
-docker save --output=n1-entity-app.tar.gz quay.io/n1analytics/entity-app:v1.5.1
+docker save --output=n1-entity-app.tar.gz quay.io/n1analytics/entity-app:v1.5.5
 docker save --output=n1-entity-nginx.tar.gz  quay.io/n1analytics/entity-nginx:v1.3.0
 docker save --output=n1-entity-db.tar.gz postgres:9.5
