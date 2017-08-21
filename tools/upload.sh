6cf132db9b532df494bfd81051318c08bb9df70e
Brian Thorne <brian@thorne.link>
2017-08-21 15:43:38 +1000

    Minor fixes to build release with jenkins
    * Log debug messages for docker-compose (CI build)
    * Tag image with branch name before upload
    * Bugfix in upload script

--- tools/upload.sh
+++ tools/upload.sh
@@ -3,6 +3,8 @@ set -e
 script_name=$(basename "$0")
 cd $(dirname "$0")
 
+export APPVERSION=$(cat ../backend/VERSION)
+
 if [ -z ${BRANCH_NAME+x} ]; then
     export BRANCH_NAME=$(git rev-parse --abbrev-ref HEAD)
 else
@@ -12,11 +14,14 @@ fi
 if [ "$BRANCH_NAME" = "master" ];
 then
     echo "Pushing images to quay.io";
-    export APPVERSION=$(cat ../backend/VERSION);
     docker push quay.io/n1analytics/entity-app:${APPVERSION};
     docker push quay.io/n1analytics/entity-nginx:v1.3.0;
 else
     echo "Uploading with label: ${BRANCH_NAME}";
+
+    docker tag quay.io/n1analytics/entity-app:${APPVERSION} quay.io/n1analytics/entity-app:${BRANCH_NAME};
+    docker tag quay.io/n1analytics/entity-nginx:v1.3.0 quay.io/n1analytics/entity-nginx:${BRANCH_NAME};
+
     docker push quay.io/n1analytics/entity-app:${BRANCH_NAME};
     docker push quay.io/n1analytics/entity-nginx:${BRANCH_NAME};
 fi
