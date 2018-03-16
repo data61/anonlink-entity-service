4c990afc703c1f87a3bca33f03ae76106ce63c8b
Brian Thorne <brian@thorne.link>
2018-03-16 14:12:33 +1100

    First basic prometheus exporter

---
+++ backend/metrics_exporter.py
@@ -0,0 +1,50 @@
+"""
+See Issue #42
+
+Version 1 could just collect metrics via the REST api:
+- mapping_count
+- mapping_ready_count
+- mapping_rate gauge
+
+"""
+import json
+import re
+import sys
+import time
+import requests
+
+from prometheus_client import start_http_server
+from prometheus_client.core import CounterMetricFamily, GaugeMetricFamily, REGISTRY
+
+
+class EntityServiceRestAPICollector(object):
+
+    def __init__(self, target):
+        self.target = target
+
+    def collect(self):
+        rate_gauge = GaugeMetricFamily('mapping_rate', 'Max number of comparisons per second')
+        mappings_gauge = CounterMetricFamily('mapping_counter_total', 'Number of mappings')
+
+        # Request the information we need from the api
+        status = requests.get(self.target + '/status').json()
+        num_mappings = status['number_mappings']
+        rate = status['rate']
+
+        rate_gauge.add_metric(['rate'], rate)
+        mappings_gauge.add_metric(['count'], num_mappings)
+
+        yield rate_gauge
+        yield mappings_gauge
+
+
+if __name__ == "__main__":
+    if len(sys.argv) < 2:
+        sys.stderr.write("Usage: metrics_exporter.py ES_API_ADDRESS LOCAL_SERVER_PORT\n")
+        sys.exit(1)
+    REGISTRY.register(EntityServiceRestAPICollector(sys.argv[1]))
+
+    start_http_server(int(sys.argv[2]))
+
+    while True:
+        time.sleep(5)
