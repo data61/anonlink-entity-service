a6c90356c4d2347c5b5a687c63b94c6997d8670b
Brian Thorne <brian@thorne.link>
2018-04-27 09:17:01 +1000

    Tests for basics of run description endpoint

--- backend/entityservice/tests/test_project_runs.py
+++ backend/entityservice/tests/test_project_runs.py
@@ -85,3 +85,42 @@ def test_posting_run_after_data_upload(requests):
         assert 'run_id' in run
         assert 'time_added' in run
         assert 'state' in run
+
+
+def test_run_description_missing_run(requests):
+    new_project_response, dp1, dp2 = create_project_upload_fake_data(requests, [1000, 1000])
+
+    r = requests.get(url + '/projects/{}/runs/{}'.format(
+        new_project_response['project_id'],
+        'invalid'
+        ),
+        headers={'Authorization': new_project_response['result_token']})
+
+    assert r.status_code == 403
+
+
+def test_run_description(requests):
+    new_project_response, dp1, dp2 = create_project_upload_fake_data(requests, [1000, 1000])
+
+    post_run_request = requests.post(
+        url + '/projects/{}/runs'.format(new_project_response['project_id']),
+        headers={'Authorization': new_project_response['result_token']},
+        json={
+            'threshold': 0.95
+        }
+    )
+
+    assert post_run_request.status_code == 201
+
+    r = requests.get(url + '/projects/{}/runs/{}'.format(
+        new_project_response['project_id'],
+        post_run_request.json()['run_id']
+        ),
+        headers={'Authorization': new_project_response['result_token']})
+
+    assert r.status_code == 200
+    run = r.json()
+
+    assert 'run_id' in run
+    assert 'notes' in run
+    assert 'threshold' in run
