605fce654eec7068127450af6a549eedfa4ffd0f
Brian Thorne <brian@thorne.link>
2018-04-27 09:30:14 +1000

    Implement run description endpoint

--- backend/entityservice/views/run.py
+++ backend/entityservice/views/run.py
@@ -10,8 +10,8 @@ import entityservice.database as db
 import entityservice.cache as cache
 from entityservice.database import get_db
 from entityservice.settings import Config as config
-from entityservice.views import abort_if_project_doesnt_exist, abort_if_invalid_results_token
 from entityservice.async_worker import check_queued_runs
+from entityservice.views.auth_checks import abort_if_run_doesnt_exist, abort_if_project_doesnt_exist, abort_if_invalid_results_token
 
 
 class Run(Resource):
@@ -22,8 +22,24 @@ class Run(Resource):
     def get(self, project_id, run_id):
 
         app.logger.info("request description of a run")
+        # Check the project and run resources exist
+        abort_if_run_doesnt_exist(project_id, run_id)
 
-        raise NotImplementedError("TODO")
+        # Check the caller has a valid results token. Yes it should be renamed.
+        abort_if_invalid_results_token(project_id, request.headers.get('Authorization'))
+
+        app.logger.info("request for run description authorized")
+
+        db_conn = db.get_db()
+        run_object = db.get_run(db_conn, run_id)
+
+        run_description_fields = {
+            'run_id': fields.String,
+            'threshold': fields.String,
+            'notes': fields.String,
+        }
+
+        return marshal(run_object, run_description_fields)
 
 
 class RunList(Resource):
