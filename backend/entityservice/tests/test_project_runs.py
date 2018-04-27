b66a9d93a5914d492a477e53446d4478b98647ca
Brian Thorne <brian@thorne.link>
2018-04-27 16:37:23 +1000

    Add tests for run's status

--- backend/entityservice/tests/test_project_runs.py
+++ backend/entityservice/tests/test_project_runs.py
@@ -1,5 +1,7 @@
+import datetime
 import time
 
+import iso8601
 import pytest
 
 from entityservice.tests.config import url
@@ -124,3 +126,101 @@ def test_run_description(requests):
     assert 'run_id' in run
     assert 'notes' in run
     assert 'threshold' in run
+
+
+def test_run_status(requests):
+    new_project_response, dp1, dp2 = create_project_upload_fake_data(requests, [1000, 1000])
+
+    post_run_request = requests.post(
+        url + '/projects/{}/runs'.format(new_project_response['project_id']),
+        headers={'Authorization': new_project_response['result_token']},
+        json={
+            'threshold': 0.90
+        }
+    )
+
+    assert post_run_request.status_code == 201
+
+    r = requests.get(url + '/projects/{}/runs/{}/status'.format(
+        new_project_response['project_id'],
+        post_run_request.json()['run_id']
+        ),
+        headers={'Authorization': new_project_response['result_token']})
+
+    assert r.status_code == 200
+    status = r.json()
+
+    assert 'state' in status
+    assert 'message' in status
+    assert 'progress' in status
+    assert 'time_added' in status
+    assert status['state'] in {'queued', 'running', 'completed'}
+
+    if status['state'] == 'running':
+        assert 'time_started' in status
+    elif status['state'] == 'completed':
+        assert 'time_started' in status
+        assert 'time_completed' in status
+
+    dt = iso8601.parse_date(status['time_added'])
+    assert datetime.datetime.now(tz=datetime.timezone.utc) - dt < datetime.timedelta(seconds=5)
+    original_progress = status['progress']['progress']
+
+    # Wait and see if the progress changes
+    time.sleep(2)
+    r = requests.get(url + '/projects/{}/runs/{}/status'.format(
+        new_project_response['project_id'],
+        post_run_request.json()['run_id']
+    ),
+                     headers={'Authorization': new_project_response['result_token']})
+    status = r.json()
+    assert status['progress']['progress'] > original_progress
+
+
+def test_run_larger(requests):
+    new_project_response, dp1, dp2 = create_project_upload_fake_data(requests, [100000, 10000])
+
+    post_run_request = requests.post(
+        url + '/projects/{}/runs'.format(new_project_response['project_id']),
+        headers={'Authorization': new_project_response['result_token']},
+        json={
+            'threshold': 0.90
+        }
+    )
+
+    assert post_run_request.status_code == 201
+
+    r = requests.get(url + '/projects/{}/runs/{}/status'.format(
+        new_project_response['project_id'],
+        post_run_request.json()['run_id']
+        ),
+        headers={'Authorization': new_project_response['result_token']})
+
+    assert r.status_code == 200
+    status = r.json()
+
+    assert 'state' in status
+    assert 'message' in status
+    assert 'progress' in status
+    assert 'time_added' in status
+    assert status['state'] in {'queued', 'running', 'completed'}
+
+    if status['state'] == 'running':
+        assert 'time_started' in status
+    elif status['state'] == 'completed':
+        assert 'time_started' in status
+        assert 'time_completed' in status
+
+    dt = iso8601.parse_date(status['time_added'])
+    assert datetime.datetime.now(tz=datetime.timezone.utc) - dt < datetime.timedelta(seconds=5)
+    original_progress = status['progress']['progress']
+
+    # Wait and see if the progress changes
+    time.sleep(30)
+    r = requests.get(url + '/projects/{}/runs/{}/status'.format(
+        new_project_response['project_id'],
+        post_run_request.json()['run_id']
+    ),
+                     headers={'Authorization': new_project_response['result_token']})
+    status = r.json()
+    assert status['progress']['progress'] > original_progress
