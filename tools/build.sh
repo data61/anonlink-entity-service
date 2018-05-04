ba4b11cb4c5e98e1d94486c14ce042718e46ed44
Hamish Ivey-Law <hamish.ivey-law@data61.csiro.au>
2018-05-04 14:14:29 +1000

    Make SRC and BUILD dirs absolute.

--- tools/build.sh
+++ tools/build.sh
@@ -8,8 +8,8 @@ export APPVERSION=$(cat backend/entityservice/VERSION)
 docker build -t quay.io/n1analytics/entity-app:latest backend
 
 # Build docs and make them available for entity-nginx
-SRC_DIR=docs
-BUILD_DIR=frontend/static
+SRC_DIR=`pwd`/docs
+BUILD_DIR=`pwd`/frontend/static
 docker build -t quay.io/n1analytics/entity-app:doc-builder docs
 docker run -it -v ${SRC_DIR}:/src -v ${BUILD_DIR}:/build quay.io/n1analytics/entity-app:doc-builder
 
