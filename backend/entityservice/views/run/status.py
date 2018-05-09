47356851a5d4c9857102215f8813014abb873ae3
wilko <wilko.henecka@data61.csiro.au>
2018-05-09 12:04:30 +1000

    removed flask marshalling, using connexion instead.

--- backend/entityservice/views/run/status.py
+++ backend/entityservice/views/run/status.py
@@ -1,5 +1,4 @@
 from flask import request
-from flask_restful import fields, marshal
 
 from entityservice import app, database as db, cache as cache
 from entityservice.database import get_db
@@ -21,35 +20,26 @@ def get(project_id, run_id):
     # return compute time elapsed and number of comparisons here
     times = db.get_run_times(dbinstance, run_id)
 
-    comparisons = cache.get_progress(run_id)
-    total_comparisons = db.get_total_comparisons_for_project(dbinstance, project_id)
-
-    progress = {
-        'total': total_comparisons,
-        'current': comparisons,
-        'progress': (comparisons / total_comparisons) if total_comparisons is not 'NA' else 0.0
-    }
-
     status = {
         "state": state,
-        "message": "running",
-        "progress": progress,
-        "time_added": times['time_added']
+        "time_added": times['time_added'],
+        "message": "Hello World"
     }
 
-    if times['time_started'] is not None:
+    if state == 'completed':
         status["time_started"] = times['time_started']
-
-    if times['time_completed'] is not None:
         status["time_completed"] = times['time_completed']
+    elif state == 'running':
 
-    run_status_fields = {
-        'state': fields.String,
-        'message': fields.String,
-        'progress': fields.Raw,
-        'time_added': fields.DateTime(dt_format='iso8601'),
-        'time_started': fields.DateTime(dt_format='iso8601'),
-        'time_completed': fields.DateTime(dt_format='iso8601'),
-    }
+        comparisons = cache.get_progress(run_id)
+        total_comparisons = db.get_total_comparisons_for_project(dbinstance, project_id)
 
-    return marshal(status, run_status_fields)
+        progress = {
+            'total': total_comparisons,
+            'current': comparisons,
+            'progress': (comparisons / total_comparisons) if total_comparisons is not 'NA' else 0.0
+        }
+        status["time_started"] = times['time_started']
+        status["progress"] = progress
+
+    return status
