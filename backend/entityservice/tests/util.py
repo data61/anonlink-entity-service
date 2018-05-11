5216548873b1df3a6dac0e490d9ed4e08613be35
Hamish Ivey-Law <426294+unzvfu@users.noreply.github.com>
2018-05-11 10:08:16 +1000

    Refactor some tests (#183)
    * Refactor 'test_project_run_results.py'.
    
    * Move two functions from 'test_project_run_results.py' to 'util.py'.
    
    * Refactor 'test_project_runs.py'.
    
    * Reduce sleep time from the "generous" *cough* amount of 30 seconds to a more reasonable 5 seconds.
    
    * Refactor 'test_project_run_status.py'.
    
    * Refactor 'test_project_run_posting.py'.
    
    * Refactor 'test_project_run_description.py'.
    
    * Refactor 'test_run_listing.py'.

--- backend/entityservice/tests/util.py
+++ backend/entityservice/tests/util.py
@@ -89,27 +89,72 @@ def create_project_upload_fake_data(requests, size, overlap=0.75, result_type='m
     return new_project_data, r1.json(), r2.json()
 
 
-def wait_for_run_completion(requests, project_id, run_id, result_token, timeout=30):
-    start_time = time.time()
-    status = get_run_status(requests, project_id, run_id, result_token)
-    while status['state'] in {'queued', 'running'} and time.time() - start_time < timeout:
-        status = get_run_status(requests, project_id, run_id, result_token)
-        time.sleep(0.1)
-
-    return status
-
-
-def get_run_status(requests, project_id, run_id, result_token):
-    r = requests.get(url + '/projects/{}/runs/{}/status'.format(
-            project_id,
-            run_id
-        ),
-        headers={'Authorization': result_token})
+def get_run_status(requests, project, run_id, result_token = None):
+    project_id = project['project_id']
+    result_token = project['result_token'] if result_token is None else result_token
+    r = requests.get(url + '/projects/{}/runs/{}/status'.format(project_id, run_id),
+                     headers={'Authorization': result_token})
 
     assert r.status_code == 200
     return r.json()
 
 
+def wait_for_run_completion(requests, project, run_id, result_token, timeout=30):
+    start_time = time.time()
+    while True:
+        status = get_run_status(requests, project, run_id, result_token)
+        if status['state'] not in {'queued', 'running'} or time.time() - start_time > timeout:
+            break
+        time.sleep(0.1)
+
+    return status
+
+
+def post_run(requests, project, threshold):
+    project_id = project['project_id']
+    result_token = project['result_token']
+
+    req = requests.post(
+        url + '/projects/{}/runs'.format(project_id),
+        headers={'Authorization': result_token},
+        json={'threshold': threshold})
+    assert req.status_code == 201
+    return req.json()['run_id']
+
+
+def get_runs(requests, project, result_token = None, expected_status = 200):
+    project_id = project['project_id']
+    result_token = project['result_token'] if result_token is None else result_token
+
+    req = requests.get(
+        url + '/projects/{}/runs'.format(project_id),
+        headers={'Authorization': result_token})
+    assert req.status_code == expected_status
+    return req.json()
+
+
+def get_run(requests, project, run_id, expected_status = 200):
+    project_id = project['project_id']
+    result_token = project['result_token']
+
+    req = requests.get(
+        url + '/projects/{}/runs/{}'.format(project_id, run_id),
+        headers={'Authorization': result_token})
+    assert req.status_code == expected_status
+    return req.json()
+
+
+def get_run_result(requests, project, run_id, result_token = None, expected_status = 200, wait=True):
+    result_token = project['result_token'] if result_token is None else result_token
+    if wait:
+        final_status = wait_for_run_completion(requests, project, run_id, result_token)
+        assert final_status['state'] == 'completed'
+
+    project_id = project['project_id']
+    r = requests.get(url + '/projects/{}/runs/{}/result'.format(project_id, run_id),
+                     headers={'Authorization': result_token})
+    assert r.status_code == expected_status
+    return r.json()
 
 
 def _check_new_project_response_fields(new_project_data):
