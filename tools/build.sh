03c723ea6dc252ab8b0f49c469c3d435e3709352
wilko <wilko.henecka@data61.csiro.au>
2018-05-14 16:24:35 +1000

    fix to build the docs container.

--- tools/build.sh
+++ tools/build.sh
@@ -8,7 +8,7 @@ export APPVERSION=$(cat backend/entityservice/VERSION)
 docker build -t quay.io/n1analytics/entity-app:latest backend
 
 # Build docs and make them available for entity-nginx
-SRC_DIR=`pwd`/docs
+SRC_DIR=`pwd`
 BUILD_DIR=`pwd`/frontend/static
 docker build -t quay.io/n1analytics/entity-app:doc-builder docs
 docker run -it -v ${SRC_DIR}:/src -v ${BUILD_DIR}:/build quay.io/n1analytics/entity-app:doc-builder
