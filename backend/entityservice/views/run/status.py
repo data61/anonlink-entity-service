d9f201b5224b434b94ced38c14ab3364885a38da
wilko <wilko.henecka@data61.csiro.au>
2018-05-23 11:27:39 +1000

    collecting info and squeezing it into new api.

--- backend/entityservice/views/run/status.py
+++ backend/entityservice/views/run/status.py
@@ -3,7 +3,8 @@ from flask import request
 from entityservice import app, database as db, cache as cache
 from entityservice.database import get_db
 from entityservice.views.auth_checks import abort_if_run_doesnt_exist, abort_if_invalid_results_token
-from entityservice.views.serialization import completed, running, RunStatus
+from entityservice.views.serialization import completed, running, error
+from entityservice.models.run import RUN_TYPES
 
 
 def get(project_id, run_id):
@@ -16,33 +17,47 @@ def get(project_id, run_id):
 
     app.logger.info("request for run status authorized")
     dbinstance = get_db()
-    is_ready, state = db.check_run_ready(dbinstance, run_id)
-
-    # return compute time elapsed and number of comparisons here
-    times = db.get_run_times(dbinstance, run_id)
-
+    run_status = db.get_run_status(dbinstance, run_id)
+    run_type = RUN_TYPES[run_status['type']]
+    state = run_status['state']
+    stage = run_status['stage']
     status = {
         "state": state,
-        "time_added": times['time_added'],
-        "message": "Hello World"
-    }
-
-    if state == 'completed':
-        status["time_started"] = times['time_started']
-        status["time_completed"] = times['time_completed']
-        return completed().dump(status)
-
-    if state == 'running':
-        comparisons = cache.get_progress(run_id)
-        total_comparisons = db.get_total_comparisons_for_project(dbinstance, project_id)
-
-        progress = {
-            'total': total_comparisons,
-            'current': comparisons,
-            'progress': (comparisons / total_comparisons) if total_comparisons is not 'NA' else 0.0
+        "time_added": run_status['time_added'],
+        "stages": run_type['stages'],
+        "current_stage": {
+            "number": stage,
+            "description": run_type['stage_descriptions'].get(stage, "there is no description for this stage")
         }
-        status["time_started"] = times['time_started']
-        status["progress"] = progress
+    }
+    # trying to get progress if available
+    if stage == 1:
+        # waiting for CLKs
+        abs_val = db.get_number_parties_uploaded(dbinstance, project_id)
+        max_val = db.get_project_column(dbinstance, project_id, 'parties')
+    elif stage == 2:
+        abs_val = cache.get_progress(run_id)
+        if abs_val is not None:
+            max_val = db.get_total_comparisons_for_project(dbinstance, project_id)
+    else: # no progress
+        abs_val = None
+    if abs_val is not None:
+        progress = {
+            'absolute': abs_val,
+            'relative': (abs_val / max_val) if max_val != 0 else 0,
+        }
+        if progress['relative'] > 1.0:
+            app.logger.warn('oh no. more than 100% ??? abs: {}, max: {}'.format(abs_val, max_val))
+        if run_status['stage'] in run_type['stage_progress_descriptions']:
+            progress['description'] = run_type['stage_progress_descriptions'][run_status['stage']]
+        status["current_stage"]["progress"] = progress
+    if state == 'completed':
+        status["time_started"] = run_status['time_started']
+        status["time_completed"] = run_status['time_completed']
+        return completed().dump(status)
+    elif state == 'running' or state == 'queued':
+        status["time_started"] = run_status['time_started']
         return running().dump(status)
-
-    return RunStatus().dump(status)
+    elif state == 'error':
+        app.logger.warn('handling the run status for state "error" is not implemented')
+        return error().dump(status)
