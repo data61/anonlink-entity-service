6e91274d5a5026f093da13529c51566c5b63a533
Brian Thorne <brian@thorne.link>
2018-05-26 18:25:56 +1000

    Add a delay before checking progress based on the size of the fixture

--- backend/entityservice/tests/test_project_runs.py
+++ backend/entityservice/tests/test_project_runs.py
@@ -3,12 +3,12 @@ import time
 
 import iso8601
 
-from entityservice.tests.config import url
 from entityservice.tests.util import create_project_upload_fake_data, has_progressed, post_run, get_run_status
 
 
 def test_run_larger(requests):
-    project, dp1, dp2 = create_project_upload_fake_data(requests, [100000, 10000])
+    size_1, size_2 = 100000, 10000
+    project, dp1, dp2 = create_project_upload_fake_data(requests, [size_1, size_2])
 
     run_id = post_run(requests, project, 0.9)
     status = get_run_status(requests, project, run_id)
@@ -23,8 +23,8 @@ def test_run_larger(requests):
     dt = iso8601.parse_date(status['time_added'])
     assert datetime.datetime.now(tz=datetime.timezone.utc) - dt < datetime.timedelta(seconds=5)
 
-    # Wait and see if the progress changes
-    time.sleep(30)
+    # Wait and see if the progress changes. This assumes a comparison rate of 10 M/s
+    time.sleep(2 + size_1 * size_2 / 10_000_000)
     status = get_run_status(requests, project, run_id)
     assert has_progressed(original_status, status)
 
