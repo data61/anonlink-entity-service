0688e3a2311e90338b56f428f311bd742a00af2b
Brian Thorne <brian@thorne.link>
2018-11-08 11:11:39 +1100

    Another database transaction

--- backend/entityservice/views/run/list.py
+++ backend/entityservice/views/run/list.py
@@ -42,21 +42,20 @@ def post(project_id, run):
     with db.DBConn() as db_conn:
         run_model.save(db_conn)
 
-    project_object = db.get_project(db_conn, project_id)
-    parties_contributed = db.get_number_parties_uploaded(db_conn, project_id)
-    log.debug("Expecting {} parties to upload data. Have received {}".format(
-        project_object['parties'], parties_contributed
-    ))
-    if parties_contributed == project_object['parties']:
-        log.info("Scheduling task to carry out all runs for project {} now".format(project_id))
-        with db_conn:
+        project_object = db.get_project(db_conn, project_id)
+        parties_contributed = db.get_number_parties_uploaded(db_conn, project_id)
+        log.debug("Expecting {} parties to upload data. Have received {}".format(
+            project_object['parties'], parties_contributed
+        ))
+        if parties_contributed == project_object['parties']:
+            log.info("Scheduling task to carry out all runs for project {} now".format(project_id))
             update_run_mark_queued(db_conn, run_model.run_id)
-        span = g.flask_tracer.get_span()
-        span.set_tag("run_id", run_model.run_id)
-        span.set_tag("project_id", run_model.project_id)
-        check_for_executable_runs.delay(project_id, serialize_span(span))
-    else:
-        log.info("Task queued but won't start until CLKs are all uploaded")
+            span = g.flask_tracer.get_span()
+            span.set_tag("run_id", run_model.run_id)
+            span.set_tag("project_id", run_model.project_id)
+            check_for_executable_runs.delay(project_id, serialize_span(span))
+        else:
+            log.info("Task queued but won't start until CLKs are all uploaded")
     return RunDescription().dump(run_model), 201
 
 
