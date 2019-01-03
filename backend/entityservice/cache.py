caedeae3c35b980eb42fdf4ab6bf405a6d6ca999
Brian Thorne <brian@thorne.link>
2019-01-03 13:40:22 +1100

    Connection timeouts are a good idea in a distributed system

--- backend/entityservice/cache.py
+++ backend/entityservice/cache.py
@@ -23,10 +23,10 @@ def connect_to_redis(read_only=False):
     """
     logger.debug("Connecting to redis")
     if config.REDIS_USE_SENTINEL:
-        sentinel = Sentinel([(redis_host, 26379)], password=redis_pass)
+        sentinel = Sentinel([(redis_host, 26379)], password=redis_pass, socket_timeout=5)
         if read_only:
             logger.debug("Looking up read only redis slave using sentinel protocol")
-            r = sentinel.slave_for('mymaster', )
+            r = sentinel.slave_for('mymaster')
         else:
             logger.debug("Looking up redis master using sentinel protocol")
             r = sentinel.master_for('mymaster')
