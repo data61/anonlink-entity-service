ae01346f10cd2cfe28b82ea5de845f9110b85a02
Brian Thorne <brian@thorne.link>
2019-09-06 09:18:20 +1000

    Retry while deleting a run

--- backend/entityservice/views/run/description.py
+++ backend/entityservice/views/run/description.py
@@ -1,6 +1,7 @@
 import psycopg2
 from flask import request
 from structlog import get_logger
+from retrying import retry
 
 from entityservice import database as db
 from entityservice.database import delete_run_data, get_similarity_file_for_run
@@ -24,20 +25,28 @@ def get(project_id, run_id):
     return RunDescription().dump(run_object)
 
 
+def _is_postgres_integrity(exc):
+    return isinstance(exc, psycopg2.IntegrityError)
+
+
+@retry(wait_exponential_multiplier=1000, wait_exponential_max=10000, retry_on_exception=_is_postgres_integrity)
+def _delete_run(run_id, log):
+    # Retry if a db integrity error occurs (e.g. when a worker is writing to the same row)
+    # Wait 2^x * 1000 milliseconds between each retry, up to 10 seconds, then 10 seconds afterwards
+    with db.DBConn() as conn:
+        log.debug("Retrieving run details from database")
+        similarity_file = get_similarity_file_for_run(conn, run_id)
+        delete_run_data(conn, run_id)
+    return similarity_file
+
+
 def delete(project_id, run_id):
     log = logger.bind(pid=project_id, rid=run_id)
     log.debug("request to delete run")
     authorize_run_detail(project_id, run_id)
     log.debug("approved request to delete run")
 
-    try:
-        with db.DBConn() as conn:
-            log.debug("Retrieving run details from database")
-            similarity_file = get_similarity_file_for_run(conn, run_id)
-            delete_run_data(conn, run_id)
-    except psycopg2.IntegrityError as e:
-        log.warning("Failed to delete run")
-        log.exception(e)
+    similarity_file = _delete_run(run_id, log)
     log.debug("Deleted run from database")
 
     if similarity_file:
