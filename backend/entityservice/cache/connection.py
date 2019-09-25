b6d921d0f72751318b0c6388a29acd81ff28f7b1
Brian Thorne <brian@thorne.link>
2019-09-25 21:04:06 +1000

    Add debug logging of redis server and port information

--- backend/entityservice/cache/connection.py
+++ backend/entityservice/cache/connection.py
@@ -7,6 +7,7 @@ from entityservice.settings import Config as config
 logger = structlog.get_logger()
 redis_host = config.REDIS_SERVER
 redis_pass = config.REDIS_PASSWORD
+redis_port = 26379
 
 
 def connect_to_redis(read_only=False):
@@ -14,9 +15,9 @@ def connect_to_redis(read_only=False):
     Get a connection to the master redis service.
 
     """
-    logger.debug("Connecting to redis")
+    logger.debug("Connecting to redis", server=redis_host, port=redis_port)
     if config.REDIS_USE_SENTINEL:
-        sentinel = Sentinel([(redis_host, 26379)], password=redis_pass, socket_timeout=5)
+        sentinel = Sentinel([(redis_host, )], password=redis_pass, socket_timeout=5)
         if read_only:
             logger.debug("Looking up read only redis slave using sentinel protocol")
             r = sentinel.slave_for('mymaster')
