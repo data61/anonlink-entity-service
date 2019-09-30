a34c65790f9471a525d971ccd2f8a40484c565ce
Brian Thorne <brian@thorne.link>
2019-09-30 20:27:10 +1000

    Remove extra call to post run task calculate_comparison_rate

--- backend/entityservice/tasks/mark_run_complete.py
+++ backend/entityservice/tasks/mark_run_complete.py
@@ -1,7 +1,7 @@
 from entityservice.async_worker import celery, logger
 from entityservice.cache.active_runs import set_run_state_complete
 from entityservice.database import DBConn, update_run_mark_complete
-from entityservice.tasks import TracedTask, calculate_comparison_rate
+from entityservice.tasks import TracedTask
 
 
 @celery.task(base=TracedTask, ignore_results=True, args_as_tags=('run_id',))
@@ -10,6 +10,5 @@ def mark_run_complete(run_id, parent_span=None):
     log.debug("Marking run complete")
     with DBConn() as db:
         update_run_mark_complete(db, run_id)
-    calculate_comparison_rate.delay()
     set_run_state_complete(run_id)
     log.info("Run marked as complete")
