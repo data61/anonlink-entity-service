104f49e4a327cf4fb4efb03d151359b5dfc0a1b4
Guillaume Smith <gwil1248@fastmail.net>
2019-11-11 16:11:35 +1100

    Tiny cleanup of a variable.

--- backend/entityservice/tests/util.py
+++ backend/entityservice/tests/util.py
@@ -434,9 +434,9 @@ def upload_binary_data(requests, data, project_id, token, count, size=128, expec
     return upload_response
 
 
-def upload_binary_data_from_file(requests, file_path, project_id, token, count, size=128, status=201):
+def upload_binary_data_from_file(requests, file_path, project_id, token, count, size=128, expected_status_code=201):
     with open(file_path, 'rb') as f:
-        return upload_binary_data(requests, f, project_id, token, count, size, status)
+        return upload_binary_data(requests, f, project_id, token, count, size, expected_status_code)
 
 
 def get_expected_number_parties(project_params):
