9dc59e19e34f2e81e3391a25102c4e2a4f85edbd
Brian Thorne <brian@hardbyte.nz>
2020-03-17 10:24:09 +1300

    Simplify solver task by using anonlink's _merge_similarities function

--- backend/entityservice/tasks/solver.py
+++ backend/entityservice/tasks/solver.py
@@ -1,13 +1,11 @@
-from array import array
-
 import anonlink
+from anonlink.candidate_generation import _merge_similarities
 
 from entityservice.object_store import connect_to_object_store
 from entityservice.async_worker import celery, logger
 from entityservice.settings import Config as config
 from entityservice.tasks.base_task import TracedTask
 from entityservice.tasks.permutation import save_and_permute
-from entityservice.utils import deduplicate_sorted_iter
 
 
 @celery.task(base=TracedTask, ignore_result=True, args_as_tags=('project_id', 'run_id'))
@@ -20,23 +18,15 @@ def solver_task(similarity_scores_filename, project_id, run_id, dataset_sizes, p
     log.debug("Creating python sparse matrix from bytes data")
     candidate_pairs_with_duplicates = anonlink.serialization.load_candidate_pairs(score_file)
     similarity_scores, (dset_is0, dset_is1), (rec_is0, rec_is1) = candidate_pairs_with_duplicates
-    float_typecode = similarity_scores.typecode
-    int_typecode = dset_is0.typecode
+
     log.info(f"Number of candidate pairs before deduplication: {len(candidate_pairs_with_duplicates[0])}")
     if len(candidate_pairs_with_duplicates[0]) > 0:
-        dedup_iter = deduplicate_sorted_iter(zip(similarity_scores, dset_is0, dset_is1, rec_is0, rec_is1))
-        # Convert back to typed arrays before solving
-        similarity_scores, dset_is0, dset_is1, rec_is0, rec_is1 = zip(*dedup_iter)
-        similarity_scores, dset_is0, dset_is1, rec_is0, rec_is1 = array(float_typecode, similarity_scores), \
-                                                                  array(int_typecode, dset_is0), \
-                                                                  array(int_typecode, dset_is1), \
-                                                                  array(int_typecode, rec_is0), \
-                                                                  array(int_typecode, rec_is1)
-        candidate_pairs = similarity_scores, (dset_is0, dset_is1), (rec_is0, rec_is1)
+        # TODO use public interface when available
+        # https://github.com/data61/anonlink/issues/271
+        candidate_pairs = _merge_similarities([zip(similarity_scores, dset_is0, dset_is1, rec_is0, rec_is1)], k=None)
         log.info(f"Number of candidate pairs after deduplication: {len(candidate_pairs[0])}")
 
         log.info("Calculating the optimal mapping from similarity matrix")
-
         groups = anonlink.solving.greedy_solve(candidate_pairs)
     else:
         groups = []
