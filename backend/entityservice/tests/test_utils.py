aa37edc3ea4d4d3329f45ce9aa1b10b55a356c6d
Brian Thorne <brian@hardbyte.nz>
2020-04-24 11:38:12 +1200

    Add utility functions to convert to common clknblocks format.

--- backend/entityservice/tests/test_utils.py
+++ backend/entityservice/tests/test_utils.py
@@ -5,6 +5,37 @@ import pytest
 from entityservice.errors import InvalidConfiguration
 from entityservice.utils import load_yaml_config
 from entityservice.tests.util import generate_bytes, temp_file_containing
+from entityservice.views import convert_encoding_upload_to_clknblock
+
+
+class TestEncodingConversionUtils:
+
+    def test_convert_encoding_upload_to_clknblock_encodings_only(self):
+
+        out = convert_encoding_upload_to_clknblock({
+            "encodings": ['123', '456', '789']
+        })
+
+        assert 'clknblocks' in out
+        assert len(out['clknblocks']) == 3
+        assert out['clknblocks'][0] == ['123', '1']
+
+    def test_convert_encoding_upload_to_clknblock(self):
+
+        out = convert_encoding_upload_to_clknblock({
+            "encodings": ['123', '456', '789', '000'],
+            "blocks": {
+                '0': ['1', '2'],
+                '1': ['1'],
+                '2': []
+            }
+        })
+
+        assert 'clknblocks' in out
+        assert len(out['clknblocks']) == 3
+        assert out['clknblocks'][0] == ['123', '1', '2']
+        assert out['clknblocks'][1] == ['456', '1']
+        assert out['clknblocks'][2] == ['789', ]
 
 
 class TestYamlLoader:
