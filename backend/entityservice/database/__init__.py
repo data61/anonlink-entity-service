a49703505bd083d7984d4d4be1d007028dad25ba
Wilko Henecka <3759021+wilko77@users.noreply.github.com>
2021-08-31 10:03:40 +1000

    Delete encodings on project delete (#683)
    * encoding gets a link to data provider, such that we can delete on cascade
    
    * also insert the dp_id into encodings
    
    * also delete data providers, which triggers deletion of encodings and blocks
    
    * fix circular import
    
    * fix connection/cursor mix-up
    
    * updated changelog

--- backend/entityservice/database/__init__.py
+++ backend/entityservice/database/__init__.py
@@ -1,9 +1,10 @@
 from .util import *
 
 from .authorization import *
+from .selections import *
 from .deletion import *
 from .insertions import *
 from .metrics import *
-from .selections import *
+
 
 from .models import models
