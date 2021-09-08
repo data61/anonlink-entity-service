65c6d8a7ff201ac818236855b1d91c247560fe2f
wilko77 <3759021+wilko77@users.noreply.github.com>
2021-09-08 19:49:38 +1000

    each dp gets its own encodings and encodingblocks table

---
+++ backend/entityservice/database/creations.py
@@ -0,0 +1,28 @@
+from structlog import get_logger
+
+
+logger = get_logger()
+
+
+def create_data_tables(conn, dp_ids):
+    log = logger.bind(dp_ids=dp_ids)
+    log.info(f'creating encodings and encodingblocks tables for dps: {dp_ids}')
+
+    with conn.cursor() as cur:
+        for dp_id in dp_ids:
+            cur.execute(f"""
+                CREATE TABLE encodings_{dp_id} (
+                    encoding_id BIGSERIAL NOT NULL, 
+                    encoding BYTEA NOT NULL, 
+                    PRIMARY KEY (encoding_id)
+                );
+                """)
+            cur.execute(f"""
+                CREATE TABLE encodingblocks_{dp_id} (
+                    entity_id INTEGER, 
+                    encoding_id BIGINT, 
+                    block_id INTEGER, 
+                    FOREIGN KEY(block_id) REFERENCES blocks (block_id), 
+                    FOREIGN KEY(encoding_id) REFERENCES encodings_{dp_id} (encoding_id) ON DELETE CASCADE
+                );
+            """)
