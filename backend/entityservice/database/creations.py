54afb9a053549a3363260eae2cd97ba644069075
wilko77 <3759021+wilko77@users.noreply.github.com>
2021-09-14 17:20:56 +1000

    use postgres advisory locks to manage concurrency

--- backend/entityservice/database/creations.py
+++ backend/entityservice/database/creations.py
@@ -7,13 +7,17 @@ def create_partitions(conn, dp_ids):
     with conn.cursor() as cur:
         for dp_id in dp_ids:
             log = logger.bind(dp_id=dp_id)
+            log.info("try to acquire lock from db")
+            sql = "SELECT pg_advisory_xact_lock(42)"
+            cur.execute(sql)
+            log.info("got the lock, let's go.")
             sql = f"CREATE TABLE encodings_{dp_id} (LIKE encodings INCLUDING DEFAULTS INCLUDING CONSTRAINTS);"
             cur.execute(sql)
             sql = f"ALTER TABLE encodings ATTACH PARTITION encodings_{dp_id} FOR VALUES IN ({dp_id});"
             cur.execute(sql)
-            log.debug(f"created partition encodings_{dp_id}")
+            log.info(f"created partition encodings_{dp_id}")
             sql = f"CREATE TABLE encodingblocks_{dp_id} (LIKE encodingblocks INCLUDING DEFAULTS INCLUDING CONSTRAINTS);"
             cur.execute(sql)
             sql = f"ALTER TABLE encodingblocks ATTACH PARTITION encodingblocks_{dp_id} FOR VALUES IN ({dp_id});"
             cur.execute(sql)
-            log.debug(f"created partition encodingblocks_{dp_id}")
+            log.info(f"created partition encodingblocks_{dp_id}")
