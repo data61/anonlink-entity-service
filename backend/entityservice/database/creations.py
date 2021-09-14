386a8c243a6f88ba1169becdf84c875c603a2a44
wilko77 <3759021+wilko77@users.noreply.github.com>
2021-09-14 11:21:44 +1000

    create new table for partition and then attach to avoid access exclusive lock.

--- backend/entityservice/database/creations.py
+++ backend/entityservice/database/creations.py
@@ -7,9 +7,13 @@ def create_partitions(conn, dp_ids):
     with conn.cursor() as cur:
         for dp_id in dp_ids:
             log = logger.bind(dp_id=dp_id)
-            sql = f"CREATE TABLE encodings_{dp_id} PARTITION OF encodings FOR VALUES IN ({dp_id});"
+            sql = f"CREATE TABLE encodings_{dp_id} (LIKE encodings INCLUDING DEFAULTS INCLUDING CONSTRAINTS);"
+            cur.execute(sql)
+            sql = f"ALTER TABLE encodings ATTACH PARTITION encodings_{dp_id} FOR VALUES IN ({dp_id});"
+            cur.execute(sql)
             log.debug(f"created partition encodings_{dp_id}")
+            sql = f"CREATE TABLE encodingblocks_{dp_id} (LIKE encodingblocks INCLUDING DEFAULTS INCLUDING CONSTRAINTS);"
             cur.execute(sql)
-            sql = f"CREATE TABLE encodingblocks_{dp_id} PARTITION OF encodingblocks FOR VALUES IN ({dp_id});"
+            sql = f"ALTER TABLE encodingblocks ATTACH PARTITION encodingblocks_{dp_id} FOR VALUES IN ({dp_id});"
+            cur.execute(sql)
             log.debug(f"created partition encodingblocks_{dp_id}")
-            cur.execute(sql)
