97fc5e0dba505a71503d634a70c32163c6b29434
wilko77 <3759021+wilko77@users.noreply.github.com>
2021-09-16 19:36:37 +1000

    you gotta keep'm separated

--- backend/entityservice/database/creations.py
+++ backend/entityservice/database/creations.py
@@ -4,17 +4,25 @@ logger = get_logger()
 
 
 def create_partitions(conn, dp_ids):
-    with conn.cursor() as cur:
-        for dp_id in dp_ids:
-            log = logger.bind(dp_id=dp_id)
-            log.info("try to acquire lock from db")
-            cur.execute("SELECT pg_advisory_xact_lock(42)")
-            log.info("got the lock, let's go.")
-            cur.execute(
-                f"CREATE TABLE encodings_{dp_id} (LIKE encodings INCLUDING DEFAULTS INCLUDING CONSTRAINTS) "
-                f"WITH (autovacuum_enabled = false)")
-            cur.execute(f"ALTER TABLE encodings ATTACH PARTITION encodings_{dp_id} FOR VALUES IN ({dp_id})")
-            cur.execute(f"CREATE TABLE encodingblocks_{dp_id} "
-                        f"(LIKE encodingblocks INCLUDING DEFAULTS INCLUDING CONSTRAINTS)")
-            cur.execute(f"ALTER TABLE encodingblocks ATTACH PARTITION encodingblocks_{dp_id} FOR VALUES IN ({dp_id})")
-            log.info(f"created partitions encodingblocks_{dp_id} and encodings_{dp_id}")
+    with conn:
+        with conn.cursor() as cur:
+            for dp_id in dp_ids:
+                log = logger.bind(dp_id=dp_id)
+                log.info("try to acquire lock from db")
+                cur.execute("SELECT pg_advisory_xact_lock(42)")
+                log.info("got the lock, let's go.")
+                cur.execute(
+                    f"CREATE TABLE encodings_{dp_id} (LIKE encodings INCLUDING DEFAULTS INCLUDING CONSTRAINTS) "
+                    f"WITH (autovacuum_enabled = false)")
+                cur.execute(f"ALTER TABLE encodings ATTACH PARTITION encodings_{dp_id} FOR VALUES IN ({dp_id})")
+    with conn:
+        with conn.cursor() as cur:
+            for dp_id in dp_ids:
+                log = logger.bind(dp_id=dp_id)
+                log.info("try to acquire lock from db")
+                cur.execute("SELECT pg_advisory_xact_lock(42)")
+                log.info("got the lock, let's go.")
+                cur.execute(f"CREATE TABLE encodingblocks_{dp_id} "
+                            f"(LIKE encodingblocks INCLUDING DEFAULTS INCLUDING CONSTRAINTS)")
+                cur.execute(f"ALTER TABLE encodingblocks ATTACH PARTITION encodingblocks_{dp_id} FOR VALUES IN ({dp_id})")
+                log.info(f"created partitions encodingblocks_{dp_id} and encodings_{dp_id}")
