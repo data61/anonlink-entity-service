6fe478e7dc6ed7c201a598af6775f827a7087687
wilko77 <3759021+wilko77@users.noreply.github.com>
2021-09-16 16:53:54 +1000

    no autovacuum for encodingblocks

--- backend/entityservice/database/creations.py
+++ backend/entityservice/database/creations.py
@@ -8,16 +8,13 @@ def create_partitions(conn, dp_ids):
         for dp_id in dp_ids:
             log = logger.bind(dp_id=dp_id)
             log.info("try to acquire lock from db")
-            sql = "SELECT pg_advisory_xact_lock(42)"
-            cur.execute(sql)
+            cur.execute("SELECT pg_advisory_xact_lock(42)")
             log.info("got the lock, let's go.")
-            sql = f"CREATE TABLE encodings_{dp_id} (LIKE encodings INCLUDING DEFAULTS INCLUDING CONSTRAINTS);"
-            cur.execute(sql)
-            sql = f"ALTER TABLE encodings ATTACH PARTITION encodings_{dp_id} FOR VALUES IN ({dp_id});"
-            cur.execute(sql)
-            log.info(f"created partition encodings_{dp_id}")
-            sql = f"CREATE TABLE encodingblocks_{dp_id} (LIKE encodingblocks INCLUDING DEFAULTS INCLUDING CONSTRAINTS);"
-            cur.execute(sql)
-            sql = f"ALTER TABLE encodingblocks ATTACH PARTITION encodingblocks_{dp_id} FOR VALUES IN ({dp_id});"
-            cur.execute(sql)
-            log.info(f"created partition encodingblocks_{dp_id}")
+            cur.execute(
+                f"CREATE TABLE encodings_{dp_id} (LIKE encodings INCLUDING DEFAULTS INCLUDING CONSTRAINTS) "
+                f"WITH (autovacuum_enabled = false)")
+            cur.execute(f"ALTER TABLE encodings ATTACH PARTITION encodings_{dp_id} FOR VALUES IN ({dp_id})")
+            cur.execute(f"CREATE TABLE encodingblocks_{dp_id} "
+                        f"(LIKE encodingblocks INCLUDING DEFAULTS INCLUDING CONSTRAINTS)")
+            cur.execute(f"ALTER TABLE encodingblocks ATTACH PARTITION encodingblocks_{dp_id} FOR VALUES IN ({dp_id})")
+            log.info(f"created partitions encodingblocks_{dp_id} and encodings_{dp_id}")
